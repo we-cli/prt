62656e36aa1ebc5b389876bbae28d2aac0ec48b6
fritx <uxfritz@163.com>
2015-07-26 16:20:03 +0800

    refactor: include 7zip for extracting

--- bin/prt.js
+++ bin/prt.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
+var url_parse = require('url').parse
 var resolve = require('path').resolve
 var extname = require('path').extname
 var basename = require('path').basename
 var dirname = require('path').dirname
 var existsSync = require('fs').existsSync
+//var unlinkSync = require('fs').unlinkSync
 var writeFileSync = require('fs').writeFileSync
 var createWriteStream = require('fs').createWriteStream
 var os_arch = require('os').arch
-var Download = require('download')
+var shell = require('shelljs')
 var glob = require('glob')
+var _7z = require('7zip')
+var Download = require('download')
 //var dwn = require('dwn')
 //var bar = require('progress-bar').create(process.stdout)
 //var mkdirpSync = require('mkdirp').sync
@@ -34,13 +38,14 @@ function do_install(name, version, callback){
   var listdir = resolve('./list/')
   var modname = resolve(listdir, name)
   if (modname.indexOf(listdir) !== 0) { // bad guy detected
-    callback(new Error('invalid target'))
+    callback(new Error('invalid pak'))
     return
   }
   try {
     var mod = require(modname)
   } catch(err) {
-    callback(new Error('target not found'))
+    console.error('pak not found')
+    callback(err)
     return
   }
   if (!version) version = mod.version_list[0] // defaults latest
@@ -48,65 +53,88 @@ function do_install(name, version, callback){
   mod.version = version // append `version`
   download(mod, function(err){
     if (err) {
+      unpack_done(err)
+      return
+    }
+    if (mod.ext === '.exe' || mod.ext === '.msi') {
+      if (mod.entry_list) { // single exe
+        unpack(mod, unpack_done)
+      } else { // installer
+        shell.exec('prts "' + mod.file + '"', { silent: true })
+        console.log('installer spawn complete')
+      }
+      return
+    } else {
+      unpack(mod, unpack_done)
+    }
+  })
+  function unpack_done(err){
+    if (err) {
       callback(err)
       return
     }
+    try {
+       link(mod)
+    } catch(err) {
+      console.error('link fail')
+      callback(err)
+      return
+    }
+    console.log('link complete')
+    callback(null)
+  }
+}
 
-    var entryfiles = mod.entry_list.reduce(function(arr, pattern){
-      var files = glob.sync(pattern, { cwd: mod.dest })
-      arr.push.apply(arr, files)
-      return arr
-    }, [])
+function link(mod){
+  var entryfiles = mod.entry_list.reduce(function(arr, pattern){
+    var files = glob.sync(pattern, { cwd: mod.dest })
+    arr.push.apply(arr, files)
+    return arr
+  }, [])
 
-    entryfiles.forEach(function(file){
-      var ext = extname(file)
-      var base = basename(file, ext)
-      var cmdfile = resolve(process.env.windir, base + '.cmd')
-      var cmd = '"' + resolve(mod.dest, file) + '" %*'
-      writeFileSync(cmdfile, cmd)
-      console.log('link created', cmdfile) // exec (blocked)
-      // would be too noisy ;)
-      //var cmdfile_ = resolve(process.env.windir, name + '_.cmd')
-      //var cmd_ = 'prt-spawn "' + cmdfile + '" %*'
-      //writeFileSync(cmdfile_, cmd_)
-      //console.log('link created', cmdfile_) // spawn (detached)
-    })
+  entryfiles.forEach(function(file){
+    var ext = extname(file)
+    var base = basename(file, ext)
+    var cmdfile = resolve(process.env.windir, base + '.cmd')
+    var cmd = '"' + resolve(mod.dest, file) + '" %*'
+    writeFileSync(cmdfile, cmd)
+    console.log('link created', cmdfile) // exec (blocked)
+    // would be too noisy ;)
+    //var cmdfile_ = resolve(process.env.windir, name + '_.cmd')
+    //var cmd_ = 'prts "' + cmdfile + '" %*'
+    //writeFileSync(cmdfile_, cmd_)
+    //console.log('link created', cmdfile_) // spawn (detached)
   })
 }
 
-/*function unpack(mod, callback){
-  var ext = extname(mod.file)
-  switch (ext) {
-    case '.zip':
-      var dir = mod.file.slice(0, -4) // `substr` not works
-      mod.dir = dir // append `dir`
-      console.log('unpacking to', dir)
-      extract_zip(mod.file, dir, function(err){
-        if (err) {
-          console.error('unpack fail')
-          callback(err)
-          return
-        }
-        console.log('unpack complete')
-        console.log(mod)
-      })
-      var zip = new AdmZip(mod.file)
-      zip.extractAllTo(dir, true)
-      console.log('unpack complete')
-      break;
-  }
-}*/
+function unpack(mod, callback){
+  // fixme: 7z -o not work
+  // fixme: cwd chdir hack
+  var _cwd = process.cwd()
+  process.chdir(mod.dest)
+  //if (!shell.which('7z')) {
+  //  callback(new Error('install 7z first'))
+  //  return
+  //}
+  // 7z -y
+  // http://superuser.com/questions/483037/extract-and-overwrite-existing-files
+  shell.exec('"' + _7z + '" x -y "' + mod.file + '"', { silent: true })
+  process.chdir(_cwd)
+  //unlinkSync(mod.file)
+  callback(null)
+}
 
 function download(mod, callback){
   var uri = mod.get_url(mod.version, os_arch())
   var dest = resolve('./cache', mod.name, mod.version)
-  var file = resolve(dest, basename(uri)
-    .replace(/\?.*/, '')) // .zip
-  var dir = file.replace(/\.zip$/, '') // dir
+  var fname = basename(url_parse(uri).pathname)
+  var file = resolve(dest, fname)
+  var ext = extname(file)
   mod.uri = uri // append `uri`
   mod.dest = dest // append `dest`
   mod.file = file // append `file`
-  if (existsSync(dest) ) {
+  mod.ext = ext // append `file`
+  if (existsSync(file) ) {
     console.log('already exist', file)
     callback(null)
     return
@@ -114,9 +142,7 @@ function download(mod, callback){
   console.log('downloading from', uri)
   console.log('saving to', file)
 
-  new Download({
-      extract: true
-    })
+  new Download({ extract: false })
     .get(uri)
     .dest(dest)
     .run(function(err){
@@ -128,19 +154,4 @@ function download(mod, callback){
       console.log('download complete')
       callback(null)
     })
-
-  /*var stream = _dwn(uri)
-  stream.on('error', function(err){
-    console.error('download fail')
-    callback(err)
-  })
-  stream.on('end', function(){
-    console.log('download complete')
-    callback(null)
-  })
-  stream.on('progress', function(v){
-    bar.update(v)
-  })
-  mkdirpSync(dirname(file))
-  stream.pipe(createWriteStream(file))*/
 }
